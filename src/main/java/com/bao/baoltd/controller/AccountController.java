1b4fbf9032e6ffebc0dce26e24d8935e692c06ae
Ben-Malik <benmaliktchamalam@gmail.com>
2021-09-26 14:03:40 +0300

    cleaned unused imports

--- src/main/java/com/bao/baoltd/controller/AccountController.java
+++ src/main/java/com/bao/baoltd/controller/AccountController.java
@@ -2,23 +2,18 @@ package com.bao.baoltd.controller;
 
 import org.springframework.stereotype.Controller;
 
-import java.security.Principal;
 import java.time.LocalTime;
 import java.util.Arrays;
-import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bao.baoltd.model.User;
