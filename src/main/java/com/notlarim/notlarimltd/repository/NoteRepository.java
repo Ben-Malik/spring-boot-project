69abd7f3f4b8ab3c0ead49628de37d0ea762931a
Ben-Malik <benmaliktchamalam@gmail.com>
2022-01-21 08:56:33 +0300

    Implemented the repository for the notes

--- src/main/java/com/notlarim/notlarimltd/repository/NoteRepository.java
+++ src/main/java/com/notlarim/notlarimltd/repository/NoteRepository.java
@@ -1,5 +1,17 @@
 package com.notlarim.notlarimltd.repository;
 
-public interface NoteRepository {
+import com.notlarim.notlarimltd.model.Note;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * A repository interface for the {@linkplain Note} entity.
+ * 
+ * @author ben-malik
+ */
+@Repository("NoteRepository")
+public interface NoteRepository extends JpaRepository<Note, Long>, JpaSpecificationExecutor<Note>{
     
 }
