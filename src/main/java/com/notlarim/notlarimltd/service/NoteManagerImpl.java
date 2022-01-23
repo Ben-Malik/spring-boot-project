0b53b03db59731291eebb9bdfedfd345dfdf72ea
Ben-Malik <benmaliktchamalam@gmail.com>
2022-01-23 06:35:05 +0300

    Started the implementation of the NoteManager interface

---
+++ src/main/java/com/notlarim/notlarimltd/service/NoteManagerImpl.java
@@ -0,0 +1,45 @@
+package com.notlarim.notlarimltd.service;
+
+import java.util.List;
+
+import com.notlarim.notlarimltd.enums.Category;
+import com.notlarim.notlarimltd.model.Note;
+
+/**
+ * An implementation to the {@linkplain NoteManager} interface.
+ * 
+ * @author ben-malik
+ */
+public class NoteManagerImpl implements NoteManager {
+
+    @Override
+    public Note getById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Note> getByCategory(Category category) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Note> getAllNotes() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Note saveNote(Note newNote) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Note deleteNote(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
