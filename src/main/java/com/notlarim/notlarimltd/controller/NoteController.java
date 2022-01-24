27e23068d8f9ab0c43a2a8d5d29aa1858bd5fc42
Ben-Malik <benmaliktchamalam@gmail.com>
2022-01-24 15:22:19 +0300

    added necessary methods to the NoteController

--- src/main/java/com/notlarim/notlarimltd/controller/NoteController.java
+++ src/main/java/com/notlarim/notlarimltd/controller/NoteController.java
@@ -8,7 +8,10 @@ import com.notlarim.notlarimltd.service.NoteManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * The controller class for the Note.
@@ -26,6 +29,26 @@ public class NoteController {
 
         List<Note> allNotes = noteManager.getAllNotes();
         model.addAttribute("allNotes", allNotes);
+
+        return "index";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteNote(Model model, Note note) {
+
+        noteManager.deleteNote(note);
+
+        return "index";
+    }
+
+    @GetMapping("note/add")
+    public String addNote(Model model, Note note) {
+
+        return "note/add";
+    }
+
+    @PostMapping("/note/add")
+    public String saveNote(Model model, Note note) {
 
         return "index";
     }
