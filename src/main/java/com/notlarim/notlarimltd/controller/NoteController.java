52242c27b71c5b254d1afaf7b3032b27c0fb4b4f
Ben-Malik <benmaliktchamalam@gmail.com>
2022-01-24 08:34:05 +0300

    Started the implementation of the NoteController

--- src/main/java/com/notlarim/notlarimltd/controller/NoteController.java
+++ src/main/java/com/notlarim/notlarimltd/controller/NoteController.java
@@ -1,5 +1,13 @@
 package com.notlarim.notlarimltd.controller;
 
+import org.springframework.stereotype.Controller;
+
+/**
+ * The controller class for the Note.
+ * 
+ * @author ben-malik
+ */
+@Controller
 public class NoteController {
     
 }
